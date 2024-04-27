afc8165ff1e6e7dbf77a339b1f6967954c0874ee
Christopher Whitley <aristurtledev@gmail.com>
2024-04-27 17:15:18 -0400

    Finish up Mac task

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -15,65 +15,26 @@ public sealed class BuildMacOSTask : BuildTaskBase
 
     public override void Run(BuildContext context)
     {
-        // var isaarch64 = RuntimeInformation.ProcessArchitecture == Architecture.Arm || RuntimeInformation.ProcessArchitecture == Architecture.aarch64;
+        var absoluteArtifactDir = context.MakeAbsolute(new DirectoryPath(context.ArtifactsDir));
+        var x8664BinaryPath = $"{absoluteArtifactDir}/osx-x86_64/bin/ffmpeg";
+        var arm64BinaryPath = $"{absoluteArtifactDir}/osx-arm64/bin/ffmpeg";
 
-        // if (context.IsUniversalBinary || !isaarch64)
-        // {
-        //     BuildOsxX64(context);
-        // }
-
-        // if (context.IsUniversalBinary || isaarch64)
-        // {
-        // BuildOsxAarch64(context);
-        // }
-    }
-
-    private static void BuildOsxX64(BuildContext context)
-    {
-        var artifactsDir = GetFullPathToArtifactDirectory(context);
-        var env = new Dictionary<string, string>
+        if (context.IsUniversalBinary)
         {
-            {"CFLAGS", $"-w -I{artifactsDir}/include -I/usr/include"},
-            {"CXXFLAGS", ""},
-            {"LDFLAGS", $"-L{artifactsDir}/lib -L/usr/lib"}
-        };
-
-        var bashCommand = @"zsh";
-        var processSettings = new ProcessSettings() { EnvironmentVariables = env };
-        var ffmpegConfigureFlags = GetFFMpegConfigureFlags(context, "osx-x64");
-        var buildFlag = GetBuildConfigure(context);
-        var hostFlag = GetHostConfigure(PlatformFamily.OSX);
-
-        BuildOgg(context, bashCommand, processSettings, buildFlag, hostFlag);
-        BuildVorbis(context, bashCommand, processSettings, buildFlag, hostFlag);
-        BuildLame(context, bashCommand, processSettings, buildFlag, hostFlag);
-        BuildFFMpeg(context, bashCommand, processSettings, ffmpegConfigureFlags);
-    }
-
-    private static void BuildOsxAarch64(BuildContext context)
-    {
-        var artifactsDir = GetFullPathToArtifactDirectory(context);
-        var env = new Dictionary<string, string>
+            // If building a universal binary, after the dependent tasks finish building hte x86_64 and arm64 binaries, 
+            // we'll need to combine them into a single binary using lipo.
+            context.StartProcess("lipo", new ProcessSettings()
+            {
+                Arguments = $"-create {x8664BinaryPath} {arm64BinaryPath} -output {absoluteArtifactDir}/ffmpeg"
+            });
+        }
+        else if (RuntimeInformation.ProcessArchitecture is Architecture.Arm or Architecture.Arm64)
         {
-            {"CFLAGS", $"-fPIC -O3  -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX13.1.sdk -I{artifactsDir}/include -I/usr/local/opt/llvm@15/include"},
-            {"CXXFLAGS", "-arch arm64"},
-            {"CPPFLAGS", "-I/usr/local/opt/llvm@15/include"},
-            {"LDFLAGS", $"-L{artifactsDir}/lib -L/usr/local/opt/llvm@15/lib/c++ -Wl,-rpath,/usr/local/opt/llvm@15/lib/c++ -arch arm64"},
-            // {"CFLAGS", $"/usr/local/opt/llvm@15/bin/clang  -arch aarch64 -w -I{artifactsDir}/include -I/usr/local/opt/llvm@15/include -I/usr/include"},
-            // {"CXXFLAGS", "/usr/local/opt/llvm@15/bin/clang++ -arch aarch64"},
-            // {"CPPFLAGS", "-I/usr/local/opt/llvm@15/include"},
-            // {"LDFLAGS", $"-L{artifactsDir}/lib -L/usr/local/opt/llvm@15/lib -L/usr/lib"}
-        };
-
-        var bashCommand = @"zsh";
-        var processSettings = new ProcessSettings() { EnvironmentVariables = env };
-        var ffmpegConfigureFlags = GetFFMpegConfigureFlags(context, "osx-aarch64");
-        var buildFlag = GetBuildConfigure(context);
-        var hostFlag = GetHostConfigure(PlatformFamily.OSX, isAarch64: true);
-
-        BuildOgg(context, bashCommand, processSettings, buildFlag, hostFlag);
-        // BuildVorbis(context, bashCommand, processSettings, buildFlag, hostFlag);
-        // BuildLame(context, bashCommand, processSettings, buildFlag, hostFlag);
-        // BuildFFMpeg(context, bashCommand, processSettings, ffmpegConfigureFlags);
+            context.CopyFile(arm64BinaryPath, $"{absoluteArtifactDir}/ffmpeg");
+        }
+        else
+        {
+            context.CopyFile(x8664BinaryPath, $"{absoluteArtifactDir}/ffmpeg");
+        }
     }
 }
