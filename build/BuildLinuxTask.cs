7f3436f06e8ddf2ac12718cd4ca512398b1db196
Christopher Whitley <aristurtledev@gmail.com>
2024-04-28 17:36:26 -0400

    WTF

--- build/BuildLinuxTask.cs
+++ build/BuildLinuxTask.cs
@@ -9,41 +9,42 @@ public sealed class BuildLinuxTask : BuildTaskBase
 
     public override void Run(BuildContext context)
     {
-        // Absolute path to the artifact directory is needed for flags since they don't allow relative path
-        var absoluteArtifactDir = context.MakeAbsolute(new DirectoryPath(context.ArtifactsDir));
+        context.CopyFile("./fakeffmpeg", "./artifacts/ffmpeg");
+        // // Absolute path to the artifact directory is needed for flags since they don't allow relative path
+        // var absoluteArtifactDir = context.MakeAbsolute(new DirectoryPath(context.ArtifactsDir));
 
-        // Generate common build directory path
-        var buildDirectory = $"{absoluteArtifactDir}/linux-x86_64";
+        // // Generate common build directory path
+        // var buildDirectory = $"{absoluteArtifactDir}/linux-x86_64";
 
-        // Create the build settings used by each library build
-        var buildSettings = new BuildSettings
-        {
-            ShellCommand = "sh",
-            PrefixFlag = buildDirectory,
-            PkgConfigPath = $"{buildDirectory}/lib/pkgconfig",
-            HostFlag = "x86_64-linux-gnu",
-            CFlags = $"-w -I{buildDirectory}/include",
-            CPPFlags = $"-I{buildDirectory}/include",
-            LDFlags = $"-L{buildDirectory}/lib --static"
-        };
+        // // Create the build settings used by each library build
+        // var buildSettings = new BuildSettings
+        // {
+        //     ShellCommand = "sh",
+        //     PrefixFlag = buildDirectory,
+        //     PkgConfigPath = $"{buildDirectory}/lib/pkgconfig",
+        //     HostFlag = "x86_64-linux-gnu",
+        //     CFlags = $"-w -I{buildDirectory}/include",
+        //     CPPFlags = $"-I{buildDirectory}/include",
+        //     LDFlags = $"-L{buildDirectory}/lib --static"
+        // };
 
-        // Get the configuration flags that will be used for the FFMpeg build
-        var ffmpegConfigureFlags = GetFFMpegConfigureFlags(context, "linux-x86_64");
+        // // Get the configuration flags that will be used for the FFMpeg build
+        // var ffmpegConfigureFlags = GetFFMpegConfigureFlags(context, "linux-x86_64");
 
-        // Build each library in correct order
-        BuildOgg(context, buildSettings);
-        BuildVorbis(context, buildSettings);
-        BuildLame(context, buildSettings);
-        BuildFFMpeg(context, buildSettings, ffmpegConfigureFlags);
+        // // Build each library in correct order
+        // BuildOgg(context, buildSettings);
+        // BuildVorbis(context, buildSettings);
+        // BuildLame(context, buildSettings);
+        // BuildFFMpeg(context, buildSettings, ffmpegConfigureFlags);
 
-        // Move the built binary from the build directory to the artifact directory
-        if(context.FileExists($"{buildDirectory}/bin/ffmpeg"))
-        {
-            context.MoveFile($"{buildDirectory}/bin/ffmpeg", $"{absoluteArtifactDir}/ffmpeg");
-        }
-        else
-        {
-            throw new FileNotFoundException("Binary doesn't exist");
-        }
+        // // Move the built binary from the build directory to the artifact directory
+        // if(context.FileExists($"{buildDirectory}/bin/ffmpeg"))
+        // {
+        //     context.MoveFile($"{buildDirectory}/bin/ffmpeg", $"{absoluteArtifactDir}/ffmpeg");
+        // }
+        // else
+        // {
+        //     throw new FileNotFoundException("Binary doesn't exist");
+        // }
     }
 }
